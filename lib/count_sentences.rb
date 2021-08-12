ebcc4f4b571d316f499286b98b5211213dda3886
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-08-11 17:22:54 -0700

    completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.include? "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.include? "?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.include? "!"
+      true
+    else
+      false
+    end
   end
 
+  delimiters = [".", "?", "!"]
   def count_sentences
-
+    # self.scan(/\w+/).length()
+    self.split(/[.!?]/).reject { |item| item.nil? || item == '' }.length()
   end
 end
+
+x = String.new()
+print "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences()
